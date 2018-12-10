78607934f647d3275ab29f825920ad6615081229
suraj.kumar <suraj.kumar@ballisticlearning.com>
2018-12-10 17:23:37 +0530

    updated version.php

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2018071202; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018071202.03; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018050800; // Requires this Moodle version.
 $plugin->component = 'tool_noblesuraj'; // Full name of the plugin (used for diagnostics).
-$plugin->release  = 'v1.2.2';
+$plugin->release  = 'v1.2.3';
 $plugin->maturity = MATURITY_ALPHA;
