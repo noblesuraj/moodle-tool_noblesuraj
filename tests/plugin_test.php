eb78ac4cd16327d305877b3b290a9f6128a2d26d
suraj.kumar <suraj.kumar@ballisticlearning.com>
2018-12-11 12:25:58 +0530

    released v2.0
    added unit sample unit test cases
    updated version.php and travis.yml

--- tests/plugin_test.php
+++ tests/plugin_test.php
@@ -15,17 +15,32 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version details.
  *
  * @package    tool_noblesuraj -- test plugin for moodle development course
  * @copyright  2018 Suraj Kumar
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
+/**
+ * Class tool_noblesuraj_plugin_testcase
+ * @group noblesuraj
+ *
+ */
 
-$plugin->version   = 2018071220; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018050800; // Requires this Moodle version.
-$plugin->component = 'tool_noblesuraj'; // Full name of the plugin (used for diagnostics).
-$plugin->release  = 'v2.0';
-$plugin->maturity = MATURITY_ALPHA;
+class tool_noblesuraj_plugin_testcase extends advanced_testcase {
+
+    public function test_adding() {
+        $this->assertEquals(2, 1+2);
+    }
+
+    public function test_deleting() {
+        global $DB;
+        $this->resetAfterTest(true);
+        $DB->delete_records('user');
+        $this->assertEmpty($DB->get_records('user'));
+    }
+    public function test_user_table_was_reset() {
+        global $DB;
+        $this->assertEquals(2, $DB->count_records('user', array()));
+    }
+}
